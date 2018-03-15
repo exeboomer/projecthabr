acd373a090a3c38d5fc6af83c3b14d217da1392a
exeboomer <otknet@yandex.ru>
2018-03-15 08:39:34 +0700

    tested

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const {sequelize} = require('./models');
+const password = require('password');
+const flash = require('connect-flash');
+const session = require('express-session');
 const port = 8000;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(session({ cookie: { maxAge: 60000 },
+                  secret: 'woot',
+                  resave: false,
+                  saveUninitialized: false}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success')
+  console.log('SU!!!')
+  res.locals.error_msg = req.flash('error')
+  console.log('noSU!!!')
+  next()
+})
 
 require('./app/routes')(app, {});
-
 
 sequelize.sync()
   .then(() => {
     app.listen(port)
     console.log(`сервер работает на ` + port + ` порту`)
+
   });
--- app/routes/register_routes.js
+++ app/routes/register_routes.js
@@ -1,11 +1,27 @@
-module.exports = function (app, db) {
+const express = require('express')
+const router = express.Router()
+const Joi = require('joi');
 
+// validation schema
+const userSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().required().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  confirmationPassword: Joi.any().valid(Joi.ref('password')).required(),
+})
+
+module.exports = function(app, db) {
   app.get('/register', (req, res) => {
-    res.render('register.ejs');
-  });
+        res.render('./register/register.ejs');
+      })
 
   app.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('Добро пожаловать в регистрацию');
+    const result = Joi.validate(req.body, userSchema)
+    console.log('result', result)
+    if (result.error) {
+      req.flash('error', 'Вы ввели разные пароли, попробуйте еще раз');
+      res.redirect('/register');
+      return;
+    }
   });
 };
